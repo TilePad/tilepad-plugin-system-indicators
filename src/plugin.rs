114a6a4a9481c0ce9964beeaf1a2ce36eda2c4df
Jacobtread <jacobtread@gmail.com>
2025-05-17 00:53:29 +1200

    feat: gpu temp indicator

--- src/plugin.rs
+++ src/plugin.rs
@@ -1,20 +1,31 @@
 use anyhow::Context;
 use lhm_client::{ComputerOptions, HardwareType, LHMClient, LHMClientHandle, Sensor, SensorType};
 use serde::{Deserialize, Serialize};
-use std::{cell::Cell, rc::Rc, time::Duration};
+use std::{
+    cell::{Cell, RefCell},
+    rc::Rc,
+    time::Duration,
+};
 use tilepad_plugin_sdk::{plugin::Plugin, session::PluginSessionHandle, tracing};
 use tokio::{
     task::{JoinHandle, spawn_local},
     time::sleep,
+    try_join,
 };
 
 #[derive(Default)]
 pub struct IndicatorsPlugin {
+    client_handle: Rc<RefCell<Option<LHMClientHandle>>>,
+
     /// Current CPU temperature value
     cpu_value: Rc<Cell<f32>>,
-
     /// Handle for the task managing CPU requests
     cpu_task: Option<JoinHandle<()>>,
+
+    /// Current GPU temperature value
+    gpu_value: Rc<Cell<f32>>,
+    /// Handle for the task managing GPU requests
+    gpu_task: Option<JoinHandle<()>>,
 }
 
 /// Message from the display
@@ -22,6 +33,7 @@ pub struct IndicatorsPlugin {
 #[serde(tag = "type", rename_all = "SCREAMING_SNAKE_CASE")]
 enum DisplayMessageIn {
     GetCpuTemp { nonce: u32 },
+    GetGpuTemp { nonce: u32 },
 }
 
 /// Message sent to the display
@@ -29,9 +41,16 @@ enum DisplayMessageIn {
 #[serde(tag = "type", rename_all = "SCREAMING_SNAKE_CASE")]
 enum DisplayMessageOut {
     CpuTemp { value: f32, nonce: u32 },
+    GpuTemp { value: f32, nonce: u32 },
 }
 
 impl Plugin for IndicatorsPlugin {
+    fn on_registered(&mut self, _session: &PluginSessionHandle) {
+        // Spawn a task to initialize the computer client handle
+        let handle = self.client_handle.clone();
+        spawn_local(init_computer(handle));
+    }
+
     fn on_display_message(
         &mut self,
         _session: &PluginSessionHandle,
@@ -48,9 +67,20 @@ impl Plugin for IndicatorsPlugin {
 
         match message {
             DisplayMessageIn::GetCpuTemp { nonce } => {
+                // No client handle is initialized
+
                 // Initialize the background task on first request
                 if self.cpu_task.is_none() {
-                    let task = spawn_local(run_computer_monitor(self.cpu_value.clone()));
+                    let client_handle = self.client_handle.borrow();
+                    let client_handle = match client_handle.as_ref() {
+                        Some(value) => value,
+                        None => return,
+                    };
+
+                    let task = spawn_local(run_cpu_sensor(
+                        client_handle.clone(),
+                        self.cpu_value.clone(),
+                    ));
                     self.cpu_task = Some(task);
                 }
 
@@ -58,37 +88,33 @@ impl Plugin for IndicatorsPlugin {
                 let value = self.cpu_value.get();
                 _ = display.send(DisplayMessageOut::CpuTemp { value, nonce });
             }
+            DisplayMessageIn::GetGpuTemp { nonce } => {
+                // No client handle is initialized
+
+                // Initialize the background task on first request
+                if self.gpu_task.is_none() {
+                    let client_handle = self.client_handle.borrow();
+                    let client_handle = match client_handle.as_ref() {
+                        Some(value) => value,
+                        None => return,
+                    };
+
+                    let task = spawn_local(run_gpu_sensor(
+                        client_handle.clone(),
+                        self.gpu_value.clone(),
+                    ));
+                    self.gpu_task = Some(task);
+                }
+
+                // Get the current value and send it back to the display
+                let value = self.gpu_value.get();
+                _ = display.send(DisplayMessageOut::GpuTemp { value, nonce });
+            }
         }
     }
 }
 
-// Find a sensor for the current CPU
-async fn get_cpu_sensor(client: &LHMClientHandle) -> anyhow::Result<Sensor> {
-    // Query for CPU hardware
-    let cpu_hardware = client.query_hardware(None, Some(HardwareType::Cpu)).await?;
-
-    // Get the first CPU hardware
-    let cpu = cpu_hardware
-        .into_iter()
-        .next()
-        .context("missing cpu hardware")?;
-
-    // Query the cpu hardware for temperature sensors
-    let sensors = client
-        .query_sensors(Some(cpu.identifier), Some(SensorType::Temperature))
-        .await?;
-
-    // Get the sensor for the CPU Package
-    let sensor = sensors
-        .into_iter()
-        .find(|sensor| sensor.name.eq("CPU Package"))
-        .context("missing cpu sensor")?;
-
-    Ok(sensor)
-}
-
-/// Run the monitoring task that polls for the CPU temperature every second
-async fn run_computer_monitor(cpu_value: Rc<Cell<f32>>) {
+async fn init_computer(handle: Rc<RefCell<Option<LHMClientHandle>>>) {
     let client = match LHMClient::connect().await {
         Ok(value) => value,
         Err(cause) => {
@@ -117,13 +143,69 @@ async fn run_computer_monitor(cpu_value: Rc<Cell<f32>>) {
         return;
     }
 
-    run_cpu_sensor(&client, cpu_value).await;
+    *handle.borrow_mut() = Some(client);
+}
+
+// Find a sensor for the current CPU
+async fn get_cpu_sensor(client: &LHMClientHandle) -> anyhow::Result<Sensor> {
+    // Query for CPU hardware
+    let cpu_hardware = client.query_hardware(None, Some(HardwareType::Cpu)).await?;
+
+    // Get the first CPU hardware
+    let cpu = cpu_hardware
+        .into_iter()
+        .next()
+        .context("missing cpu hardware")?;
+
+    // Query the cpu hardware for temperature sensors
+    let sensors = client
+        .query_sensors(Some(cpu.identifier), Some(SensorType::Temperature))
+        .await?;
+
+    // Get the sensor for the CPU Package
+    let sensor = sensors
+        .into_iter()
+        .find(|sensor| sensor.name.eq("CPU Package"))
+        .context("missing cpu sensor")?;
+
+    Ok(sensor)
+}
+
+// Find a sensor for the current CPU
+async fn get_gpu_sensor(client: &LHMClientHandle) -> anyhow::Result<Sensor> {
+    // Query for GPU hardware
+    let (gpu_nvidia, gpu_amd, gpu_intel) = try_join!(
+        client.query_hardware(None, Some(HardwareType::GpuNvidia)),
+        client.query_hardware(None, Some(HardwareType::GpuAmd)),
+        client.query_hardware(None, Some(HardwareType::GpuIntel)),
+    )?;
+
+    // Get the first GPU hardware
+    let gpu = gpu_nvidia
+        .into_iter()
+        .chain(gpu_amd.into_iter())
+        .chain(gpu_intel.into_iter())
+        .next()
+        .context("missing gpu")?;
+
+    // Query the cpu hardware for temperature sensors
+    let sensors = client
+        .query_sensors(Some(gpu.identifier), Some(SensorType::Temperature))
+        .await?;
+
+    // Get the sensor for the CPU Package
+    let sensor = sensors
+        .into_iter()
+        .find(|sensor| sensor.name.eq("GPU Core"))
+        .context("missing gpu sensor")?;
+
+    Ok(sensor)
 }
 
 /// Run a loop for the CPU sensor storing its current temperature value in `cpu_value`
-async fn run_cpu_sensor(client: &LHMClientHandle, cpu_value: Rc<Cell<f32>>) {
+async fn run_cpu_sensor(client: LHMClientHandle, cpu_value: Rc<Cell<f32>>) {
     // Get the CPU sensor
-    let mut cpu_sensor = match get_cpu_sensor(client).await {
+    let mut cpu_sensor = match get_cpu_sensor(&client).await {
         Ok(value) => value,
         Err(cause) => {
             tracing::error!(?cause, "failed to obtain cpu sensor");
@@ -141,7 +223,7 @@ async fn run_cpu_sensor(client: &LHMClientHandle, cpu_value: Rc<Cell<f32>>) {
             // CPU sensor was lost (Another client refreshed cache?)
             Ok(None) => {
                 // Try and obtain the CPU sensor again
-                if let Ok(value) = get_cpu_sensor(client).await {
+                if let Ok(value) = get_cpu_sensor(&client).await {
                     cpu_sensor = value;
                     continue;
                 }
@@ -159,6 +241,51 @@ async fn run_cpu_sensor(client: &LHMClientHandle, cpu_value: Rc<Cell<f32>>) {
 
         // Update the current temperature value
         cpu_value.set(value);
+
+        // Wait till the next tick
+        sleep(Duration::from_secs(1)).await;
+    }
+}
+
+/// Run a loop for the CPU sensor storing its current temperature value in `cpu_value`
+async fn run_gpu_sensor(client: LHMClientHandle, gpu_value: Rc<Cell<f32>>) {
+    // Get the CPU sensor
+    let mut gpu_sensor = match get_gpu_sensor(&client).await {
+        Ok(value) => value,
+        Err(cause) => {
+            tracing::error!(?cause, "failed to obtain cpu sensor");
+            return;
+        }
+    };
+
+    loop {
+        // Get the current value of the CPUs sensor
+        let value = match client
+            .get_sensor_value_by_id(gpu_sensor.identifier.clone(), true)
+            .await
+        {
+            Ok(Some(value)) => value,
+            // CPU sensor was lost (Another client refreshed cache?)
+            Ok(None) => {
+                // Try and obtain the CPU sensor again
+                if let Ok(value) = get_gpu_sensor(&client).await {
+                    gpu_sensor = value;
+                    continue;
+                }
+
+                // Some other factor is preventing us from gaining the CPU sensor
+                tracing::warn!("cpu temperature sensor no longer exists");
+                return;
+            }
+
+            Err(cause) => {
+                tracing::error!(?cause, "failed to get current temperature value");
+                return;
+            }
+        };
+
+        // Update the current temperature value
+        gpu_value.set(value);
 
         // Wait till the next tick
         sleep(Duration::from_secs(1)).await;
